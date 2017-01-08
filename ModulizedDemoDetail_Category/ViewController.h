9fd8cbf19ffd1fd94a591dc0c3b4057e1845c03c
TobyoTenma <tmtbo@hotmail.com>
2017-01-08 23:38:21 +0800

    1.0

---
+++ ModulizedDemoDetail_Category/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ModulizedDemoDetail_Category
+//
+//  Created by TobyoTenma on 08/01/2017.
+//  Copyright © 2017 TobyoTenma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ readme.md
@@ -0,0 +1,1 @@
+pod "ModulizedDemoDetail_Category"
